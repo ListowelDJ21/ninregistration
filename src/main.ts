465fe87050dacc5428ebcbbb1fdfc4602b736c54
ListowelDJ21 <davidlistowelj@gmail.com>
2021-03-05 20:53:44 +0100

    ca3

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { dirname, join } from 'path';
+import * as nunjucks from 'nunjucks';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule,
+  );
+
+  const express = app.getHttpAdapter().getInstance();
+  const views = join(__dirname, '..', 'views');
+  nunjucks.configure(views, { express });
+
+  const staticAssets = join(__dirname, '..', 'static');
+  app.useStaticAssets(staticAssets);
+
   await app.listen(3000);
 }
 bootstrap();
